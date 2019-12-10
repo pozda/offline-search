885f7facd46a71b07d5e05cabbd9e6e5fa41289e
Ivan Pozderac <ivan.pozderac@gmail.com>
2019-12-10 04:11:51 +0100

    setup eslint with prettier and flow, delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-//@flow 
-
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
